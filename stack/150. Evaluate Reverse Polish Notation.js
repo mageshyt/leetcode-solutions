c4185763e780b7ac37923379e13a98104bdb8fb2
MAGESH H <hariarunengg@gmail.com>
2022-04-05 00:51:49 +0530

    leetcode problem 150 solution added

---
+++ stack/150. Evaluate Reverse Polish Notation.js
@@ -0,0 +1,41 @@
+`Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+
+Valid operators are +, -, *, and /. Each operand may be an integer or another expression.
+
+Note that division between two integers should truncate toward zero.
+
+It is guaranteed that the given RPN expression is always valid. That means the expression would always evaluate to a result, and there will not be any division by zero operation.
+
+ 
+
+Example 1:
+
+Input: tokens = ["2","1","+","3","*"]
+Output: 9
+Explanation: ((2 + 1) * 3) = 9
+Example 2:
+
+Input: tokens = ["4","13","5","/","+"]
+Output: 6
+Explanation: (4 + (13 / 5)) = 6`;
+
+const evalRPN = (tokens) => {
+  const stack = [];
+  for (let i = 0; i < tokens.length; i++) {
+    const curr = tokens[i];
+    if (curr == "+") {
+      stack.push(stack.pop() + stack.pop());
+    } else if (curr == "-") {
+      stack.push(-stack.pop() + stack.pop());
+    } else if (curr == "*") {
+      stack.push(stack.pop() * stack.pop());
+    } else if (curr == "/") {
+      stack.push(Math.trunc(stack.pop() / stack.pop()));
+    } else {
+      stack.push(parseInt(curr));
+    }
+  }
+  return stack[0];
+};
+
+console.log(evalRPN(["2", "1", "+", "3", "*"]));
