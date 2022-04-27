62e43d38b818e98f0178dc0fb899250e4d85e6e1
magesh <hariarunengg@gmail.com>
2022-04-27 23:20:15 +0530

    solved some dp problems

--- two pointers/189. Rotate Array.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def rotate(self, nums, k) :
-        for i in range(k):
-            nums[:] = nums[-1:] + nums[:-1]
-            print('After -->', nums)
-        return nums
-
-test=Solution()
-print(test.rotate([1,2,3,4,5,6,7],3))
