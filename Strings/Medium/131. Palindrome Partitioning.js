5ee306b130dc1a342bb46508f5095aca075f2815
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2023-01-22 22:25:30 +0530

    backtracking problem solved

--- Strings/Medium/131. Palindrome Partitioning.js
+++ Strings/Medium/131. Palindrome Partitioning.js
@@ -13,4 +13,31 @@ Example 2:
 Input: s = "a"
 Output: [["a"]]
  `;
- 
+
+const isPalindrome = (s) => s === s.split("").reverse().join("");
+
+const partition = (s) => {
+  const res = [];
+  const part = []; // current partition
+
+  const dfs = (start) => {
+    if (start >= s.length) {
+      res.push([...part]);
+      return;
+    }
+
+    for (let i = start; i < s.length; i++) {
+      if (isPalindrome(s.substring(start, i + 1))) {
+        part.push(s.substring(start, i + 1));
+        dfs(i + 1);
+        part.pop();
+      }
+    }
+  };
+
+  dfs(0);
+
+  return res;
+};
+
+console.log("👉 Output:", partition("aab"));
