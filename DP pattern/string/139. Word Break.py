a765a91dc2381c503242eb7bd6acb2172f8010a7
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2023-06-21 15:56:41 +0530

    work break

--- DP pattern/string/139. Word Break.py
+++ DP pattern/string/139. Word Break.py
@@ -4,32 +4,31 @@ class Solution:
         words = {}
         for word in wordDict:
             words[word] = 1
+        print(words)
+        dp = {}
 
-        dp={}
         def helper(i: int, words):
-
             # base case
+            print(dp)
             if i == len(s):
                 return True
 
             if i in dp:
                 return dp[i]
-            
+
             dp[i] = False
             # recursive case
 
             for j in range(i+1, len(s)+1):
                 if s[i:j] in words and helper(j, words):
                     dp[i] = True
-                    return True
-                
-            return False
+
+            return dp[i]
         return helper(0, words)
 
 
-s = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab"
+s = "leetcode"
 
-wordDict = ["a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa",
-            "aaaaaaa", "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa"]
+wordDict = ["leet", "code"]
 
 print(Solution().wordBreak(s, wordDict))
