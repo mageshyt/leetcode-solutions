ff52c5edfaf52e593ae2061c01dc2513ebb6c6d1
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2023-06-22 22:14:09 +0530

    pascal problem solved

---
+++ Dynamic programmming/python/119. Pascal's Triangle II.py
@@ -0,0 +1,34 @@
+from typing import List
+
+
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        # base case
+
+        if rowIndex == 0:
+            return [1]
+
+        if rowIndex == 1:
+
+            return [1, 1]
+
+        # recursive case
+        def helper(row):
+            if row == 1:
+                return [1, 1]
+            else:
+                prev_row = helper(row-1)
+                new_row = [1]
+                for i in range(1, len(prev_row)):
+                    # now we add the previous row's elements
+                    new_row.append(prev_row[i-1]+prev_row[i])
+
+                new_row.append(1)
+                return new_row
+
+        return helper(rowIndex)
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.getRow(3))
