83de01d4e6ae3566edfcd632c6612f08611f2187
MAGESH H <hariarunengg@gmail.com>
2023-07-24 22:14:21 +0530

    problem solved

---
+++ JAVASCRIPT/day9.js
@@ -0,0 +1,16 @@
+`2623. Memoize`;
+
+function memoize(fn) {
+  const cache = {};
+  return function (...args) {
+    const key = JSON.stringify(args);
+
+    if (key in cache) {
+      return cache[key];
+    }
+
+    const result = fn.apply(this, args);
+    cache[key] = result;
+    return result;
+  };
+}
