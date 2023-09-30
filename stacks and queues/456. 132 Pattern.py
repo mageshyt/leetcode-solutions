a334ef914129e46c5601159d563f64887dd2c4c0
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2023-09-30 21:09:11 +0530

    find patter 123 problem solved

---
+++ stacks and queues/456. 132 Pattern.py
@@ -0,0 +1,54 @@
+"""Given an array of n integers nums, a 132 pattern is a subsequence of three integers nums[i], nums[j] and nums[k] such that i < j < k and nums[i] < nums[k] < nums[j].
+
+Return true if there is a 132 pattern in nums, otherwise, return false.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: false
+Explanation: There is no 132 pattern in the sequence.
+Example 2:
+
+Input: nums = [3,1,4,2]
+Output: true
+Explanation: There is a 132 pattern in the sequence: [1, 4, 2].
+Example 3:
+
+Input: nums = [-1,3,2,0]
+Output: true
+Explanation: There are three 132 patterns in the sequence: [-1, 3, 2], [-1, 3, 0] and [-1, 2, 0].
+ 
+
+"""
+from typing import List
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        stack=[[]] # stack of pairs [num,min before]
+
+        minBefore=nums[0]
+
+        for i in range(1,len(nums)):
+            # remove all pairs with minBefore>=nums[i]
+
+            while stack and stack[-1][0]>=nums[i]:
+                stack.pop()
+
+
+            # if stack is not empty, we have minBefore<nums[i]
+            if stack and stack[-1][1]<nums[i]:
+                return True
+            
+            # add nums[i] to stack
+            stack.append([nums[i],minBefore])
+
+            # update minBefore
+            minBefore=min(minBefore,nums[i])
+
+        return False
+    
+
+
+
+#  [3,1,4,2]
