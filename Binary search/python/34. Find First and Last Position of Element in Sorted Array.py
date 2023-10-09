d3881edad10a9d36b6a24c2846535a7418d58e1e
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2023-10-09 23:05:16 +0530

    problem 34.find first and last position

--- Binary search/python/34. Find First and Last Position of Element in Sorted Array.py
+++ Binary search/python/34. Find First and Last Position of Element in Sorted Array.py
@@ -1,41 +1,61 @@
+
+""""""
+from typing import List
 class Solution:
-    def searchRange(self, nums, target: int) :
-        if not nums: return [-1, -1]
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        # Check if the array is empty
+        if not nums:
+            return [-1, -1]
 
-        res=[]
+        result = []
 
-        def binarySearch(target,left,right):
+        # Binary search helper function to find the target
+        def binarySearch(target: int, left: int, right: int) -> int:
             while left <= right:
-                mid=left+(right-left)//2
-                if nums[mid]==target:
+                mid = left + (right - left) // 2
+
+                # Case 1: If the mid element is the target, return the index
+                if nums[mid] == target:
                     return mid
-                if nums[mid]>target:
-                    right=mid-1
+
+                # Case 2: If the mid element is greater than the target, search in the left side (move right pointer)
+                elif nums[mid] > target:
+                    right = mid - 1
                 else:
-                    left=mid+1
+                    # Case 3: If mid element is less than the target, search in the right side (move left pointer)
+                    left = mid + 1
+
+            # If target is not found, return -1
             return -1
-        firstIdx=binarySearch(target,0,len(nums)-1)
-        if firstIdx == -1 :
-            return [-1,-1]
+    
+        # Find the starting index of the target number
+        firstIdx = binarySearch(target, 0, len(nums) - 1)
 
-        start=firstIdx
+        # If first index is not found, return [-1, -1]
+        if firstIdx == -1:
+            return [-1, -1]
 
-        end=firstIdx
+        endIdx = firstIdx
+        start = firstIdx
+        temp1 = 0
 
-        temp1=0
-
+        # Find the starting index of the target number
         while start != -1:
-            temp1=start
-            start=binarySearch(target,0,temp1-1)
-        
-        start=temp1
+            temp1 = start
+            start = binarySearch(target, 0, temp1 - 1)
 
-        temp2=0
-        while end != -1:
-            temp2=end
-            end=binarySearch(target,temp2+1,len(nums)-1)
+        start = temp1
+        temp2 = 0
 
-        return  [temp1,temp2]
+        # Find the ending index of the target number
+        while endIdx != -1:
+            temp2 = endIdx
+            endIdx = binarySearch(target, temp2 + 1, len(nums) - 1)
+
+        endIdx = temp2
+
+        return [start, endIdx]
+
 
 
 
