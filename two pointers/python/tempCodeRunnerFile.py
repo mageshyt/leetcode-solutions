bd62139212f17c54306f2068d890ba4d4eb08e0d
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2023-12-30 22:18:55 +0530

    :pencil: Add new files and update code in Strings and two pointers directories

---
+++ two pointers/python/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+                        if p2 < len(colors) and  colors[p1] != colors[p2]:
+                            print("Not Matching",colors[p1],colors[p2])
+                            p1=p2
+                            p2+=1
