5f492bb8fcd42517ae3369159cc497cbd5c574d2
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-01-22 22:37:24 +0530

    Refactor code to improve performance and readability

---
+++ Others/645. Set Mismatch.py
@@ -0,0 +1,20 @@
+from typing import List
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        res=[0,0]
+
+        for n in nums:
+            num=abs(n)
+            if nums[num-1]<0:
+                res[0]=num
+            else:
+                nums[num-1]=-nums[num-1]
+
+        for i in range(len(nums)):
+            if nums[i]>0:
+                res[1]=i+1
+                break
+        return res
+    
+
+        
