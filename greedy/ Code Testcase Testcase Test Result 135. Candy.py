3815a6f90a1b7da6886c63efe5dc75c147447e5f
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-01-24 16:56:12 +0530

    Add solution for candy problem

---
+++ greedy/ Code Testcase Testcase Test Result 135. Candy.py
@@ -0,0 +1,17 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        candies=[1]*len(ratings)
+        for i in range(1,len(ratings)):
+            if ratings[i] > ratings[i-1]:
+                candies[i]=candies[i-1]+1
+
+                
+
+        res=candies[-1]
+
+        for i in reversed(range(len(ratings)-1)):
+            if ratings[i] > ratings[i+1]:
+                candies[i]=max(candies[i],candies[i+1]+1)
+            res+=candies[i]
+
+        return res
