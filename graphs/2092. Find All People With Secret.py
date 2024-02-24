cbc791013beb62804c78a860051b0f5f4d2da951
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-02-24 22:34:06 +0530

    2092 problem solved

---
+++ graphs/2092. Find All People With Secret.py
@@ -0,0 +1,37 @@
+from collections import defaultdict
+
+
+class Solution:
+    def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
+        secrets=set([0,firstPerson]) # people with secrete
+
+        # basesd on time
+        time_map={} # time:meetings
+        for src,dist , t in meetings:
+            if t not in time_map:
+                time_map[t]=defaultdict(list)
+            time_map[t][src].append(dist)
+            time_map[t][dist].append(src)
+
+        def dfs(src,adj):
+            if src in visted:
+                return
+            visted.add(src)
+            secrets.add(src)
+
+            for nei in adj[src]:
+                if nei not in visted:
+                    dfs(nei,t)
+
+
+
+        for t in sorted(time_map.keys()):
+            visted=set()
+            # consider as source node
+            for src in time_map[t]:
+
+                if src in secrets:
+                    dfs(src,time_map[t])
+
+        # print(time_map)
+        return list(secrets)
