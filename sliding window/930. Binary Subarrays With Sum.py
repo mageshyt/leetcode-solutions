02e31e2b3423ec1ab26df217a3b1a6eee39c1b70
MAGESH H <hariarunengg@gmail.com>
2024-04-03 22:15:41 +0530

    Refactor sliding window solution for binary subarrays with sum

--- sliding window/930. Binary Subarrays With Sum.py
+++ sliding window/930. Binary Subarrays With Sum.py
@@ -1,34 +1,4 @@
 from typing import List
-<<<<<<< HEAD
-class Solution:
-    # Time : O(n) Space: O(1)
-    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-
-        def helper(goal):
-
-            if goal < 0:
-                return 0
-            
-            res=0
-            left,curr=0,0
-
-            for r in range(len(nums)):
-                curr+=nums[r]
-                while curr > goal:
-                    curr-=nums[left]
-                    left+=1
-                # add size of subarray
-                res+=r-left+1
-            return res
-
-
-        return helper(goal)-helper(goal-1)
-
-    
-
-sol=Solution()
-print(sol.numSubarraysWithSum([0,0,0,0,0],0))
-=======
 
 
 class Solution:
@@ -42,4 +12,3 @@ class Solution:
 
 
 print(Solution().numSubarraysWithSum([1, 0, 1, 0, 1], 2))
->>>>>>> 167b9ae (sliding window problem)
