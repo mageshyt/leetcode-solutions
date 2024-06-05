e893b661dea721eb717378de4656223dbf0b5ece
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-06-05 21:13:23 +0530

    problem  1002 find comman char solved

---
+++ Strings/Easy/1002. Find Common Characters.py
@@ -0,0 +1,29 @@
+"""
+Given a string array words, return an array of all characters that show up in all strings within the words (including duplicates). You may return the answer in any order.
+
+
+
+Example 1:
+
+Input: words = ["bella","label","roller"]
+Output: ["e","l","l"]
+Example 2:
+
+Input: words = ["cool","lock","cook"]
+Output: ["c","o"]
+
+"""
+
+from typing import List
+from collections import Counter
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        res = Counter(words[0])
+        for word in words[1:]:
+            res &= Counter(word)
+
+        return list(res.elements())
+
+
+print(Solution().commonChars(["bella","label","roller"])) # ["e","l","l"]
+print(Solution().commonChars(["cool","lock","cook"])) # ["c","o"]
