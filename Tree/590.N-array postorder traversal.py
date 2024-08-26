3edd815654f3b8d46da2204eadcc77813b552e78
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-08-26 23:17:11 +0530

    solved n array tree post orxder

---
+++ Tree/590.N-array postorder traversal.py
@@ -0,0 +1,27 @@
+"""
+# Definition for a Node.
+"""
+
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+
+from typing import List
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        # base case
+        if not root:
+            return []
+
+        res = []
+
+        for child in root.children:
+            res.extend(self.postorder(child))
+
+        res.append(root.val)
+
+        return res
+
+
+
