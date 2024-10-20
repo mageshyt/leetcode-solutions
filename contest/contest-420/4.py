c9a5773286a1ad5f5e710cd82b191c0d43658a15
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-10-20 09:46:23 +0530

    Add Solution class with methods to check if a string is a palindrome and find answers based on parent-child relationships

---
+++ contest/contest-420/4.py
@@ -0,0 +1,30 @@
+from typing import List
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        return s == s[::-1]
+    
+    def dfs(self, x: int, children: List[List[int]], s: str) -> None:
+        for y in sorted(children[x]):  
+            self.dfs(y, children, s)
+        self.dfsStr += s[x]  
+    
+    def findAnswer(self, parent: List[int], s: str) -> List[bool]:
+        n = len(parent)
+        
+        children = [[] for _ in range(n)]
+        for i in range(1, n):
+            children[parent[i]].append(i)
+        
+        answer = [False] * n
+        
+        for i in range(n):
+            self.dfsStr = ""  
+            self.dfs(i, children, s)
+            
+            if self.isPalindrome(self.dfsStr):
+                answer[i] = True
+        
+        return answer
+
+
+
