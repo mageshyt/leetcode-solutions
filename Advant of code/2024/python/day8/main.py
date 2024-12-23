b4c0243686712f231a78da24caed70a23f76a842
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-12-23 23:08:19 +0530

    advet of day 8 solved backtacking problem

---
+++ Advant of code/2024/python/day8/main.py
@@ -0,0 +1,16 @@
+"""
+"""
+# ========================= PART 1 =========================
+
+# ========================= PART 2 =========================
+
+
+
+
+
+import sys
+if sys.argv[1]=="test":
+    test_ip=open("./testcase.txt","r").read().strip().split("\n")
+else:
+    test_ip=open("./input.txt","r").read().strip().split("\n")
+
