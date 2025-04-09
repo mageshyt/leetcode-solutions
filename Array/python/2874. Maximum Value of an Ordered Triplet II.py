32c93aedfa54ad91908cd376d50515ab4405a9c7
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2025-04-09 20:16:24 +0530

    maximum value of an ordered tripled solved

--- Array/python/2874. Maximum Value of an Ordered Triplet II.py
+++ Array/python/2874. Maximum Value of an Ordered Triplet II.py
@@ -1,17 +1,14 @@
-
 from typing import List
 class Solution:
 
-        def maximumTripletValue(self, nums: List[int]) -> int:
-                
-                res=0
-                max_left=0
-                max_right=0
-                for num in nums:
-                        res= max(res, num*max_right)
-                        max_right=max(max_right,max_left-num)
-                        max_left=max(max_left,num)
+    def maximumTripletValue(self, nums: List[int]) -> int:
+            
+        res=0
+        max_left=0
+        max_right=0
+        for num in nums:
+                res= max(res, num*max_right)
+                max_right=max(max_right,max_left-num)
+                max_left=max(max_left,num)
 
-                return res
-        
-
+        return res
