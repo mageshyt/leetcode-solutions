9f289279dcfe4fbb9e40b82b80c421098d3af394
mageshyt <mageshytdev@gmail.com>
2025-10-08 22:45:46 +0530

    water bottles 2 simulation problem fixed

---
+++ simulation/3100. Water Bottles II.py
@@ -0,0 +1,53 @@
+"""
+You are given two integers numBottles and numExchange.
+
+numBottles represents the number of full water bottles that you initially have. In one operation, you can perform one of the following operations:
+
+Drink any number of full water bottles turning them into empty bottles.
+Exchange numExchange empty bottles with one full water bottle. Then, increase numExchange by one.
+Note that you cannot exchange multiple batches of empty bottles for the same value of numExchange. For example, if numBottles == 3 and numExchange == 1, you cannot exchange 3 empty water bottles for 3 full bottles.
+
+Return the maximum number of water bottles you can drink.
+
+ 
+
+Example 1:
+
+
+Input: numBottles = 13, numExchange = 6
+Output: 15
+Explanation: The table above shows the number of full water bottles, empty water bottles, the value of numExchange, and the number of bottles drunk.
+Example 2:
+
+
+Input: numBottles = 10, numExchange = 3
+Output: 13
+Explanation: The table above shows the number of full water bottles, empty water bottles, the value of numExchange, and the number of bottles drunk.
+ 
+
+Constraints:
+
+1 <= numBottles <= 100 
+1 <= numExchange <= 100
+"""
+
+class Solution:
+    def maxBottlesDrunk(self, numBottles: int, numExchange: int) -> int:
+        full = numBottles
+        empty = 0
+        exchange = numExchange
+        drunk = 0
+        
+        while full > 0:
+            # Drink all full bottles
+            drunk += full
+            empty += full
+            full = 0
+            
+            # Exchange empty bottles for full ones if possible
+            while empty >= exchange:
+                full += 1
+                empty -= exchange
+                exchange += 1
+        
+        return drunk
