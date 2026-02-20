403cecd3512af6eadead6c0bc95485fcd6334751
mageshyt <mageshytdev@gmail.com>
2026-02-20 21:50:06 +0530

    speical binary string sovled
    try to sovled this again

---
+++ bits/761. Special Binary String.py
@@ -0,0 +1,49 @@
+"""
+Special binary strings are binary strings with the following two properties:
+
+The number of 0's is equal to the number of 1's.
+Every prefix of the binary string has at least as many 1's as 0's.
+You are given a special binary string s.
+
+A move consists of choosing two consecutive, non-empty, special substrings of s, and swapping them. Two strings are consecutive if the last character of the first string is exactly one index before the first character of the second string.
+
+Return the lexicographically largest resulting string possible after applying the mentioned operations on the string.
+
+ 
+
+Example 1:
+
+Input: s = "11011000"
+Output: "11100100"
+Explanation: The strings "10" [occuring at s[1]] and "1100" [at s[3]] are swapped.
+This is the lexicographically largest string possible after some number of swaps.
+Example 2:
+
+Input: s = "10"
+Output: "10"
+"""
+
+class Solution:
+    def makeLargestSpecial(self, s: str) -> str:
+        if not s:
+            return ""
+
+        count = 0
+        i = 0
+        special_substrings = []
+
+        for j, char in enumerate(s):
+            count += 1 if char == '1' else -1
+            print('[j,char,count]',j,char,count,'special_substrings',special_substrings)
+            if count == 0:
+                print('## [j,char,count]',j,char,count,'special_substrings',special_substrings)
+                special_substrings.append('1' + self.makeLargestSpecial(s[i+1:j]) + '0')
+                i = j + 1
+
+        special_substrings.sort(reverse=True)
+        return ''.join(special_substrings)
+
+
+if __name__ == "__main__":
+    s = "11011000"
+    print(Solution().makeLargestSpecial(s))  # Output: "11100100"
