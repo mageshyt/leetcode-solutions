580d8e14cfd9a503f338d413c644e257ad3cfc30
mageshyt <mageshytdev@gmail.com>
2026-03-25 22:06:11 +0530

    solved contruct product matrix 2

---
+++ 2-d Array/2906. Construct Product Matrix.py
@@ -0,0 +1,30 @@
+
+from typing import List
+class Solution:
+    def constructProductMatrix(self, grid: List[List[int]]) -> List[List[int]]:
+        MOD = 12345
+        m, n = len(grid), len(grid[0])
+        res = [[0] * n for _ in range(m)]
+
+        suffix = 1
+
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                res[i][j] = suffix
+                suffix = (suffix * grid[i][j]) % MOD
+
+        prefix = 1
+
+        for i in range(m):
+            for j in range(n):
+                res[i][j] = (res[i][j] * prefix) % MOD
+                prefix = (prefix * grid[i][j]) % MOD
+
+        return res
+
+
+
+
+if __name__ == "__main__":
+    grid = [[12345],[2],[1]]
+    print(Solution().constructProductMatrix(grid))
