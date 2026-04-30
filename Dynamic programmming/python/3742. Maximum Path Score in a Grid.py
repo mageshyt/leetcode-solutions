beedb1bbd78ae7a3875465fb73c82c96a61aaf70
mageshyt <mageshytdev@gmail.com>
2026-04-30 21:44:29 +0530

    dp:sovled max path screo problem

---
+++ Dynamic programmming/python/3742. Maximum Path Score in a Grid.py
@@ -0,0 +1,86 @@
+"""
+You are given an m x n grid where each cell contains one of the values 0, 1, or 2. You are also given an integer k.
+
+You start from the top-left corner (0, 0) and want to reach the bottom-right corner (m - 1, n - 1) by moving only right or down.
+
+Each cell contributes a specific score and incurs an associated cost, according to their cell values:
+
+0: adds 0 to your score and costs 0.
+1: adds 1 to your score and costs 1.
+2: adds 2 to your score and costs 1. ​​​​​​​
+Return the maximum score achievable without exceeding a total cost of k, or -1 if no valid path exists.
+
+Note: If you reach the last cell but the total cost exceeds k, the path is invalid.
+
+ 
+
+Example 1:
+
+Input: grid = [[0, 1],[2, 0]], k = 1
+
+Output: 2
+
+Explanation:​​​​​​​
+
+The optimal path is:
+
+Cell	grid[i][j]	Score	Total
+Score	Cost	Total
+Cost
+(0, 0)	0	0	0	0	0
+(1, 0)	2	2	2	1	1
+(1, 1)	0	0	2	0	1
+Thus, the maximum possible score is 2.
+
+Example 2:
+
+Input: grid = [[0, 1],[1, 2]], k = 1
+
+Output: -1
+
+Explanation:
+
+There is no path that reaches cell (1, 1)​​​​​​​ without exceeding cost k. Thus, the answer is -1.
+
+ 
+
+Constraints:
+
+1 <= m, n <= 200
+0 <= k <= 103​​​​​​​
+​​​​​​​grid[0][0] == 0
+0 <= grid[i][j] <= 2    
+"""
+from typing import List
+
+class Solution:
+    def maxPathScore(self, grid: List[List[int]], k: int) -> int:
+        rows, cols = len(grid), len(grid[0])
+
+        dp = {}
+
+        def dfs(r, c, cost):
+
+            if (r, c, cost) in dp:
+                return dp[(r, c, cost)]
+
+            if (r, c) == (rows - 1, cols - 1):
+                return grid[r][c] if cost + (grid[r][c] > 0) <= k else float('-inf')
+
+            score = float('-inf')
+            for dr, dc in [(0, 1), (1, 0)]:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < rows and 0 <= nc < cols:
+                    new_cost = cost + (grid[nr][nc] > 0)
+                    if new_cost <= k:
+                        score = max(score, grid[r][c] + dfs(nr, nc, new_cost))
+
+            dp[(r, c, cost)] = score
+
+            return score
+
+        result = dfs(0, 0, 0)
+
+        return result if result != float('-inf') else -1
+
+
